6c36e6a4f4d12338decb14d60dab47ad19187e5c
ChocoSoda <azrael513@gmail.com>
2022-12-05 17:39:38 +0900

    updated

--- app.py
+++ app.py
@@ -27,21 +27,48 @@ def todo_post():
     db.todo.insert_one(doc)
 
     return jsonify({'msg' : '할 일 등록'})
+@app.route('/todo/edit', methods=["POST"])
+def todo_edit():
+    todo_receives = request.form['todo_gives']
+    db.todo.update_one({'todo': todo_receives}, {'$set':{'todo' : 'value'}})
+    return jsonify({'msg' : '수정 완료'})
+# 여기 해야하는데 왜 성공할 기미가 없냐
+
 @app.route('/todo', methods=["GET"])
 def todo_get():
     todo_list = list(db.todo.find({}, {'_id':False}))
     return jsonify({'todos' : todo_list})
+
+@app.route('/todo/left', methods=["GET"])
+def todo_count():
+    left_item = list(db.todo.find({'done'}, {'_id':False}))
+    count = len(left_item)
+
+    return jsonify({'lefts' : count})
+
+# 일단 포기
 @app.route('/todo/done', methods=["POST"])
 def todo_done():
     num_receive = request.form['num_give']
     db.todo.update_one({'num':int(num_receive)}, {'$set':{'done':1}})
     return jsonify({'msg':'할 일 완료'})
+@app.route('/todo/all', methods=["POST"])
+def all_done():
+    done_receive = request.form['done_give']
+    db.todo.update_many({'done': int(done_receive)}, {'$set':{'done':1}})
+    return jsonify({'msg' : '할 일 모두 완료'})
 
 @app.route('/todo/del', methods=["POST"])
 def todo_del():
     num_receive = request.form['num_give']
     db.todo.delete_one({'num':int(num_receive)})
     return jsonify({'msg' : '삭제 완료'})
+
+@app.route('/todo/del_all', methods=["POST"])
+def all_del():
+    done_receives = request.form['done_give']
+    db.todo.delete_many({'done': int(done_receives)})
+    return jsonify({'msg' : '모두 삭제 완료'})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
